f1727bac342c546ad1eadaadbaccbf891808da3e
AstralSol <porphyrios33@gmail.com>
2020-10-06 20:44:48 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = 1
+def batch_badge_creator(speaker)
+    speaker.each do |speak|
+        puts "Hello, #{speak}! You'll be assigned to room #{room}!"
+        room += 1
+    end
+end
